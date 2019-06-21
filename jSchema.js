9d349ec4acf6d7f2edccf90343e9076732f0f357
Om Prakaash <omsuseela@gmail.com>
2019-06-21 16:06:36 -0700

    Working parser - Single datapoint

--- jSchema.js
+++ jSchema.js
@@ -1,5 +1,5 @@
 const generateSchema = require('generate-schema');
-const ejs        = require('easy-json-schema')
+const ejs            = require('easy-json-schema')
 const fs             = require('fs');
 
 // Reading targer json file
@@ -11,9 +11,75 @@ fs.readFile(process.argv[2], (err, data)=>{
     console.log(data)
 
     var jsonData = JSON.parse(data)
-    var schema = generateSchema.json("JSON-Schema", jsonData)
-    var schema_1 = ejs(jsonData)
-    console.log(schema)
-    console.log(JSON.stringify(schema_1,null, 2))
+    schema = {}
+    Parse(jsonData, schema)
+
+    console.log(JSON.stringify(schema))
 
 })
+
+function Parse(data, schema){
+    if (Array.isArray(data)){
+        schema['type'] = "Array" // Root Level Type = Array
+        ParseArray(data, schema)
+    }
+    else{
+        ParseObject(data, schema)
+    }
+}
+
+function ParseObject(object, schema){
+    for (var key in object){
+
+        // Literal Value
+        if (typeof object[key] != 'object'){
+            console.log(key, object[key])
+            schema[key] = typeof object[key]
+        }
+        else if (typeof object[key] == 'object'){  // If nested Object or Array
+            
+            // Nested Object
+            if ( ! Array.isArray(object[key])){
+                nestedSchema = {}
+                schema[key] = nestedSchema
+                ParseObject(object[key], nestedSchema)
+            }
+            // Array
+            else{
+                console.log("Correct")
+                list = ['array']
+                schema[key] = list
+                console.log(object[key])
+                ParseArray(object[key], list )
+            }
+
+        }       
+
+    }
+
+}
+
+function ParseArray(array, list){
+    console.log(array)
+    for (var i = 0; i < array.length; i++){
+        var item = array[i]
+        if (typeof item != 'object'){ // Literal Value
+            console.log(item)
+            list[1] = typeof item
+        }
+        else if (Array.isArray(item)){ // Nested Array
+            console.log('Wrong')
+            nestedList = ['array']
+            list[1] = nestedList
+            ParseArray(item, nestedList)
+        }
+        else{   // Object type
+            console.log('Dict in array')
+            nestedSchema = {}
+            list.push(nestedSchema)
+            ParseObject(item, nestedSchema)
+        }
+    }
+
+}
+
